6e1516fcf4b590a5dd964d8c85d38a3c9c9d41ec
Tarun Suneja <suneja.tarun@gmail.com>
2020-09-16 00:30:22 +0530

    change ui

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
           >
             Learn React
           </a>
+          <h2> Fib Calculator</h2>
 
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
